56e83ba9ef618d483fc9576de8fbd0bda0aa6d80
M45ter <yaowen8885@163.com>
2020-01-17 21:23:28 +0800

    update

--- News/app/src/main/java/com/zey/news/MainActivity.java
+++ News/app/src/main/java/com/zey/news/MainActivity.java
@@ -1,14 +1,28 @@
 package com.zey.news;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.zey.news.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //set toolbar
+        setSupportActionBar(mBinding.toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("头条");
+        }
     }
 }
